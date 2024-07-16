d3fa747ff17fba1ac44b937652d643029442ac5a
webdevgenius2014 <webdevgenius2014@gmail.com>
2024-07-16 10:47:43 +0530

    fix media path

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { isAuthenticated } from '@/helper/functions'
 import store from '@/store'
-// import Media  from '@/views/Admin/Media-section/Index.vue'
+import Media  from '../views/Admin/Media-section/Index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -167,12 +167,12 @@ const router = createRouter({
           props: true,
           meta: { requiresAuth: true }
         },
-        // {
-        //   path: '/media',
-        //   name: 'Media',
-        //   component: Media,
-        //   meta: { requiresAuth: true }
-        // },
+        {
+          path: '/media',
+          name: 'Media',
+          component: Media,
+          meta: { requiresAuth: true }
+        },
         {
           path: '/Contract-design',
           name: 'Contract',
