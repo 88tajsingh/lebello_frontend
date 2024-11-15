83b76af5bb251b6e63edc1d9968c57e11ee57fba
webdevgenius <webdevgenius2014@gmail.com>
2024-11-15 12:42:24 +0530

    product detail page update

---
+++ src/views/forntend-views/ProductDetailNew.vue
@@ -0,0 +1,11 @@
+<template>
+ <div class="mb-5">
+    <NavBar :absolute="false" />
+  </div>
+</template>
+
+<script setup>
+import NavBar from '@/components/frontend-components/Nav-bar.vue';
+
+
+</script>
