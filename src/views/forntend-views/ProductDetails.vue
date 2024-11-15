dcb7b6e201de063580e8ad66717bb7a17954a076
webdevgenius <webdevgenius2014@gmail.com>
2024-11-15 17:21:27 +0530

    product Detail design update

--- src/views/forntend-views/ProductDetails.vue
+++ src/views/forntend-views/ProductDetails.vue
@@ -259,10 +259,10 @@
               alt="Social Icon" /> </a> </li>
       </ul> -->
 
-      <div class="relative top-30 right-0 pr-3 self-center" ref="closeMenu2">
+      <!-- <div class="relative top-30 right-0 pr-3 self-center" ref="closeMenu2">
         <SideMenu :list="list" :handleSideMenu="handleSideMenu2" :isOpen="isOpenSidebarSlider2" :mainSlider="false"
           :showDropDown="false" :showHeading="false" :downDropdown="true" />
-      </div>
+      </div> -->
     </div>
 
 
