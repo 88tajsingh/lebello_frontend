633de5dbb88c9364a5e5250b6b7a5fd51d0ae99f
webdevgenius <webdevgenius2014@gmail.com>
2024-12-20 19:01:14 +0530

    routing according slug

--- src/views/forntend-views/store/Storepage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<NavBar :absolute />
-</template>
-<script setup >
-import NavBar from '@/components/frontend-components/Nav-bar.vue';
-import AccordionNew from '@/components/frontend-components/AccordionNew.vue';
-</script>
